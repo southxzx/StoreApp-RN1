12809bdad6350c37ac11dc2d0f534b538f338072
southxz <phuongnamsk109@gmail.com>
2021-02-19 22:15:16 +0700

    Flatlist

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { StyleSheet, Text, View, ScrollView, FlatList } from 'react-native';
 import CategoryListItem from './component/CategoryListItem';
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <CategoryListItem/>
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cate: [
+        {id: 1, name:"Weather forecast"},
+        {id: 2, name:"Weather forecast"},
+        {id: 3, name:"Weather forecast"},
+        {id: 4, name:"Weather forecast"}
+      ]
+    }
+  }
+  render() {
+    const {cate} = this.state;
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={cate}
+          renderItem = {({item})=><CategoryListItem cate={item}/>}
+          keyExtractor = {item => `${item.id}`}
+          contentContainerStyle={{paddingRight: 16, paddingLeft: 16}}
+        />
       <StatusBar style="auto" />
     </View>
-  );
+    )
+  }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'stretch',
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
 });
+
+
+
--- component/CategoryListItem.js
+++ component/CategoryListItem.js
@@ -3,14 +3,40 @@ import {
     Image,
     View,
     Text,
+    StyleSheet
 } from 'react-native';
 
 import WeatherImage from '../assets/weather-app.png';
 
-export default function CategoryListItem(){
-    return <View>
-        <Text>CategoryListItem</Text>
-        <Image source={WeatherImage}/>
-        <Text>SSS</Text>
+export default function CategoryListItem(props){
+    const { cate }=props;
+    return (
+    <View style={styles.container}>
+        <Text style={styles.title}>{cate.name}</Text>
+        <Image style={styles.categoryImage} source={WeatherImage}/>
     </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    categoryImage: {
+        width: 64,
+        height: 64
+    },
+    title:{
+        textTransform: 'uppercase',
+        marginBottom: 8,
+        fontWeight: '700'
+    },
+    container: {
+        alignItems: 'center',
+        padding: 16,
+        borderRadius: 5,
+        shadowColor: '#000',
+        shadowOpacity: 0.1,
+        shadowRadius: 10,
+        shadowOffset: { width: 0, height: 0 },
+        backgroundColor: '#fff',
+        marginBottom: 16
+    }
+})
